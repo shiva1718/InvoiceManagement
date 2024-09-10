d68f23d476c729f2cae0dc7d6f0fc0911b30ba0b
shiva <shiva.sankar@zohocorp.com>
2024-09-10 15:47:40 +0530

    removed profile option in home page

---
+++ src/main/java/com/shiva/invoicemanagement/controllers/ReportsController.java
@@ -0,0 +1,10 @@
+package com.shiva.invoicemanagement.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/invoices")
+public class ReportsController {
+
+}
